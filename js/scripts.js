fc73efc1b59fed0988226b27bcd6dc19d4625f12
gissandrogama <gissandrogama@gmail.com>
2020-05-17 03:29:46 -0300

    active dos links menus

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+//mudação de seção suave
+const menuItems = document.querySelectorAll('.menu a[href^="#"]');
+const activeclass = 'active'
+
+menuItems.forEach(item => {
+    item.addEventListener('click', scrollToId);
+})
+
+
+
+function getScrollTopByHref(element) {
+    const id = element.getAttribute('href');
+    return document.querySelector(id).offsetTop;
+}
+
+function scrollToId(event) {
+    event.preventDefault();
+    menuItems.forEach(btn => btn.classList.remove(activeclass));
+    this.classList.add(activeclass)
+    const to = getScrollTopByHref(event.target) - 43;    
+    scrollPosition(to);    
+}
+
+function scrollPosition(to) {
+    window.scroll({
+        top: to,
+        behavior: "smooth",
+    })
+}
